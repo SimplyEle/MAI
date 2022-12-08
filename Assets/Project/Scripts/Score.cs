0bab6b843c3050f75f27b3b2a925a9f208b0732f
SimplyEle <helenminec@gmail.com>
2022-12-08 19:47:31 +0300

    add second part of score system

--- Assets/Project/Scripts/Score.cs
+++ Assets/Project/Scripts/Score.cs
@@ -1,23 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Score : MonoBehaviour
 {
     private int score;
+    public TextMeshProUGUI scoreText;
 
     void Start()
     {
         score = 0;
-    }
-
-    void OnGUI()
-    {
-        GUI.Label(new Rect(10, 10, 100, 100), "Score: "+ score);
+        AddScore(0);
     }
     
     public void AddScore(int points)
     {
         score += points;
+        scoreText.text = score.ToString();
     }
 }

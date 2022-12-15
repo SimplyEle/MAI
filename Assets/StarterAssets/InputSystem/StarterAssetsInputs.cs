4a4284f909c4b4c57c9427ec7ad6b0ff48a02324
SimplyEle <helenminec@gmail.com>
2022-12-15 20:37:56 +0300

    delete player's moving

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -8,10 +8,7 @@ namespace StarterAssets
 	public class StarterAssetsInputs : MonoBehaviour
 	{
 		[Header("Character Input Values")]
-		public Vector2 move;
 		public Vector2 look;
-		public bool jump;
-		public bool sprint;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -21,10 +18,6 @@ namespace StarterAssets
 		public bool cursorInputForLook = true;
 
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
-		public void OnMove(InputValue value)
-		{
-			MoveInput(value.Get<Vector2>());
-		}
 
 		public void OnLook(InputValue value)
 		{
@@ -34,36 +27,11 @@ namespace StarterAssets
 			}
 		}
 
-		public void OnJump(InputValue value)
-		{
-			JumpInput(value.isPressed);
-		}
-
-		public void OnSprint(InputValue value)
-		{
-			SprintInput(value.isPressed);
-		}
 #endif
-
-
-		public void MoveInput(Vector2 newMoveDirection)
-		{
-			move = newMoveDirection;
-		} 
 
 		public void LookInput(Vector2 newLookDirection)
 		{
 			look = newLookDirection;
-		}
-
-		public void JumpInput(bool newJumpState)
-		{
-			jump = newJumpState;
-		}
-
-		public void SprintInput(bool newSprintState)
-		{
-			sprint = newSprintState;
 		}
 		
 		private void OnApplicationFocus(bool hasFocus)
--- Assets/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
+++ Assets/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
@@ -8,24 +8,9 @@ namespace StarterAssets
         [Header("Output")]
         public StarterAssetsInputs starterAssetsInputs;
 
-        public void VirtualMoveInput(Vector2 virtualMoveDirection)
-        {
-            starterAssetsInputs.MoveInput(virtualMoveDirection);
-        }
-
         public void VirtualLookInput(Vector2 virtualLookDirection)
         {
             starterAssetsInputs.LookInput(virtualLookDirection);
-        }
-
-        public void VirtualJumpInput(bool virtualJumpState)
-        {
-            starterAssetsInputs.JumpInput(virtualJumpState);
-        }
-
-        public void VirtualSprintInput(bool virtualSprintState)
-        {
-            starterAssetsInputs.SprintInput(virtualSprintState);
         }
         
     }

20b5378d393f25d2919f9cb86e9e4e2e5cf27e9a
SimplyEle <helenminec@gmail.com>
2023-04-10 11:42:09 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 export DB_HOST=127.0.0.1
 export DB_PORT=3360
-export DB_LOGIN=stud
-export DB_PASSWORD=stud
-export DB_DATABASE=archdb
+export DB_LOGIN=cnfuser
+export DB_PASSWORD=cnfuser
+export DB_DATABASE=cnfdb
 
 ./build/arch_of_software
 

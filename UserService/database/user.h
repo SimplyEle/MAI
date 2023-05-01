1e970bcd08110b02102b4948522235dacfcb8aa4
SimplyEle <helenminec@gmail.com>
2023-05-01 15:33:11 +0300

    update

--- UserService/database/user.h
+++ UserService/database/user.h
@@ -11,6 +11,7 @@ namespace database
     class User{
         private:
             long _id;
+            long _main_id;
             std::string _first_name;
             std::string _last_name;
             std::string _email;
@@ -23,6 +24,7 @@ namespace database
             static User fromJSON(const std::string & str);
 
             long             get_id() const;
+            long get_main_id() const;
             const std::string &get_first_name() const;
             const std::string &get_last_name() const;
             const std::string &get_email() const;
@@ -31,6 +33,7 @@ namespace database
             const std::string &get_password() const;
 
             long&        id();
+            long& main_id();
             std::string &first_name();
             std::string &last_name();
             std::string &email();
@@ -44,6 +47,9 @@ namespace database
             static std::vector<User> read_all();
             static std::vector<User> search(std::string first_name,std::string last_name);
             void save_to_mysql();
+
+            void save_to_cache();
+            static std::optional<User> read_from_cache_by_id(long id);
 
             Poco::JSON::Object::Ptr toJSON() const;
 
